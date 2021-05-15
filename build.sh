b82e51a3530842e80c82357b058d73019893ba6c
Sami Laine <sami.v.laine@gmail.com>
2021-05-16 01:20:06 +0300

    Update build script

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
-makepbo Zeus_yymmdd_Template.Stratis Zeus_yymmdd_Template-$(git describe).Stratis
+#!/bin/bash
+set -euo pipefail
+for x in makepbo.linux makepbo makepbo.exe; do
+  if command -v $x &> /dev/null; then
+    cmd=$x
+  fi
+done
+if [ -z "${cmd:-}" ]; then
+  >&2 echo makepbo not found
+  exit 1
+fi
+$cmd -N Zeus_yymmdd_Template.Stratis Zeus_yymmdd_Template-$(git describe).Stratis
